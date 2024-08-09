1c5a147d437e382a5c79ff984d587f8eaabba292
SubashMallireddy <subash@tonic.com.au>
2024-08-09 19:06:54 +1000

    add form for searching gifs

--- src/components/GiphySearch/GiphySearch.tsx
+++ src/components/GiphySearch/GiphySearch.tsx
@@ -1,6 +1,20 @@
+import './GiphySearch.css'
 const GiphySearch = () => {
+    const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const formObject = Object.fromEntries(formData.entries());
+        console.log("🚀 ~ GiphySearch ~ formObject:", formObject);
+    }
+
     return (
-        <div>Giphy Search</div>
+        <div className='wrapper'>
+            <form onSubmit={handleSubmit} className='search-form'>
+                <input type="text" className="search-input" placeholder='Find the perfect GIF to express your mood!' name='q' />
+                <button>Search</button>
+            </form>
+
+        </div>
     )
 }
 
