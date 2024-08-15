245513b95128b34c31a0fab485251bf02a039600
SubashMallireddy <subash@tonic.com.au>
2024-08-15 15:26:42 +1000

    experiment masonary layout with react-virtualized library

--- src/components/GiphySearch/GiphySearch.tsx
+++ src/components/GiphySearch/GiphySearch.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './GiphySearch.css'
-import useSearchForGifs from '../../hooks/useSearchForGifs';
+import GiphysMasonary from './GiphysMasonary';
 const GiphySearch = () => {
 
     const [queryString, setQueryString] = useState('');
     const [shouldSearch, setshouldSearch] = useState(false);
+    const gifsGridContainer = useRef<HTMLDivElement>(null);
 
     const handleSearchQueryChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
         e.stopPropagation();
@@ -20,24 +21,14 @@ const GiphySearch = () => {
         setshouldSearch(false);
     }
 
-    const { data, loading, error } = useSearchForGifs({ queryString, shouldSearch, resetShouldSearch });
-
-    if (loading) return <>`loading...`</>
-    if (error) return <>`error: ${error.message}`</>
-
-    if (data) console.log(data)
 
     return (
-        <div className='wrapper'>
+        < div className='wrapper'>
             <form onSubmit={handleSubmit} className='search-form'>
                 <input type="text" className="search-input" placeholder='Find the perfect GIF to express your mood!' value={queryString} onChange={handleSearchQueryChange} />
                 <button>Search</button>
             </form>
-            <div className='search-output-wrapper'>
-                {
-                    data && queryString && <h3>Search results for <em>{`${queryString}`}</em></h3>
-                }
-            </div>
+            <GiphysMasonary queryString={queryString} shouldSearch={shouldSearch} resetShouldSearch={resetShouldSearch} />
         </div>
     )
 }
@@ -45,5 +36,6 @@ const GiphySearch = () => {
 export default GiphySearch;
 
 /**
- * 1. 
+ * 1. the most important thing is to decide how many columns you want to display on the screen based on the screen size.
+ * 2. Remember the image width is 200px
  */
---
+++ src/components/GiphySearch/GiphysMasonary/GiphysMasonary.tsx
@@ -0,0 +1,156 @@
+import { GiphyObject } from "../../../hooks/useSearchGiphys/types";
+import useSearchForGifs from "../../../hooks/useSearchGiphys";
+import "./GiphysMasonary.css";
+import {
+    CellMeasurer,
+    CellMeasurerCache,
+    createMasonryCellPositioner,
+    Masonry,
+    AutoSizer
+} from 'react-virtualized';
+
+import ImageMeasurer from 'react-virtualized-image-measurer';
+import { useCallback, useEffect, useMemo, useRef } from "react";
+
+interface GifsGridProps {
+    queryString: string,
+    shouldSearch: boolean,
+    resetShouldSearch: () => void;
+}
+
+const GIF_FIXED_SMALL_WIDTH = 200;
+// * Just an assumed value
+const GIF_FIXED_SMALL_HEIGHT = 100;
+
+const GifsGrid = ({ queryString, shouldSearch, resetShouldSearch }: GifsGridProps) => {
+
+    const { response, loading, error } = useSearchForGifs({ queryString, shouldSearch, resetShouldSearch });
+    // const containerRef = useRef<HTMLDivElement>(null);
+
+    if (error) return <>`error: ${error.message}`</>
+    if (loading) return <>loading...</>
+
+    if (response) console.log(response);
+
+
+    return (
+        // <div className='giphys-masonary'>
+        //     {
+        //         response && response.data && (
+        //             <>
+        //                 {/* <h3>Search results for <em>{`${queryString}`}</em></h3>
+        //                 {
+        //                     response.data.map(giphy => {
+        //                         return <Gif {...giphy} key={giphy.id} />
+        //                     })
+        //                 } */}
+        //                 <Masonry items={response.data} render={Gif} columnWidth={GIF_FIXED_SMALL_WIDTH} overscanBy={1} columnGutter={8} />
+        //             </>
+        //         )
+        //     }
+        // </div>
+        <>
+            {
+                response && response.data && (
+                    <div className="giphys-masonary" >
+
+                        <ImageMeasurer
+                            items={response.data}
+                            image={giphy => giphy.images.fixed_width.url}
+                            defaultHeight={GIF_FIXED_SMALL_HEIGHT}
+                            defaultWidth={GIF_FIXED_SMALL_WIDTH}>
+                            {({ itemsWithSizes }) => <MasonryComponent itemsWithSizes={itemsWithSizes} />}
+                        </ImageMeasurer>
+                    </div>
+                )
+            }
+        </>
+    )
+}
+
+export default GifsGrid;
+
+type GiphyData = Partial<Pick<GiphyObject, 'id' | 'url' | 'images' | 'bitly_gif_url' | 'bitly_url' | 'alt_text' | 'title'>>;
+
+interface GiphyProps {
+    index: number;
+    data: GiphyData;
+    width: number;
+}
+// eslint-disable-next-line
+const Gif = ({ index, width, data: { id, url, images, bitly_gif_url, bitly_url, title, alt_text: altText } }: GiphyProps) => {
+    return (
+        <div className="giphy" key={`${index}-${id}`}>
+            <a href={url}>
+                {/* <figure> */}
+                {/* // eslint-disable-next-line */}
+                <img src={images.fixed_width.url} alt={altText} />
+                {/* <figcaption>{title || '-'}</figcaption> */}
+                {/* </figure> */}
+            </a>
+        </div>
+    )
+}
+
+
+// Default sizes help Masonry decide how many images to batch-measure
+const cache = new CellMeasurerCache({
+    defaultHeight: 250,
+    defaultWidth: 200,
+    fixedWidth: true,
+});
+
+const cellPositioner = createMasonryCellPositioner({
+    cellMeasurerCache: cache,
+    columnCount: 5,
+    columnWidth: GIF_FIXED_SMALL_WIDTH,
+    spacer: 10,
+})
+
+const MasonryComponent = ({ itemsWithSizes }) => {
+    console.log("🚀 ~ MasonryComponent ~ itemsWithSizes:", itemsWithSizes)
+    const cellRenderer = useCallback(function cellRenderer({ index, key, parent, style }) {
+        const { item, size } = itemsWithSizes[index];
+        // const columnWidth = GIF_FIXED_SMALL_WIDTH;
+        // const defaultHeight = GIF_FIXED_SMALL_HEIGHT;
+        // const height = columnWidth * (size.height / size.width) || defaultHeight;
+
+        return (
+            <CellMeasurer cache={cache} index={index} key={`${key}-${item.id}`} parent={parent}>
+                <div style={style}>
+                    <img
+                        src={item.images.fixed_width.url}
+                        alt={item.alt_text}
+                        style={{
+                            height: item.images.fixed_width.width,
+                            width: item.images.fixed_width.height,
+                        }}
+                    />
+                    {/* <h4>{item.title}</h4> */}
+                </div>
+            </CellMeasurer>
+        );
+    }, [itemsWithSizes])
+
+
+
+    console.log("🚀 ~ MasonryComponent ~ cellPositioner:", cellPositioner)
+    return (
+        <div className="satti">
+
+            <AutoSizer>
+                {({ height, width }) => (
+                    <Masonry
+                        cellCount={itemsWithSizes.length}
+                        cellMeasurerCache={cache}
+                        cellPositioner={cellPositioner}
+                        cellRenderer={cellRenderer}
+                        height={height}
+                        width={width}
+
+                    />
+                )}
+            </AutoSizer>
+        </div>
+    )
+}
